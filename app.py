8e2decc53270a6bcab4b605435add8b7a311a5b8
Kiriharan Mohan <it21200410@my.sliit.lk>
2023-10-20 23:04:41 +0530

    Backend tests done

--- app.py
+++ app.py
@@ -27,27 +27,23 @@ def add_articles():
 def predict_manuja():
     Junction = request.json['junction']
     date = request.json['date']
-    # date = datetime.datetime.strptime(date,'%Y-%m-%d')
     time = request.json['time']
+    time = int(str(time)[:2])
     df = preProcess(Junction, date, time)
     traffic_level = model.predict(df)
-    return {traffic_level[0]}
+    return str(traffic_level[0])
 
 
 def preProcess(junction, date, time):
-    inputs = {'Junction': [junction], 'date': [date], 'time': [time]}       
+    inputs = {'Junction': [junction], 'date': [date], 'traffic_time': [time]}       
     df = pd.DataFrame(inputs)
     df['date'] = pd.to_datetime(df['date'])
     df['week'] = df['date'].apply(week_of_month)
     df['day'] = df['date'].dt.day_name()
-    df['traffic_time'] = df['time'].apply(lambda x: datetime.datetime.strptime(x, '%H:%M').strftime('%H:%M'))
-    df['traffic_time'] = int(str(df['time'])[:2])                                           
+    # df['traffic_time'] = df['time'].apply(lambda x: datetime.datetime.strptime(x, '%H:%M').strftime('%H:%M'))
+    # df['traffic_time'] = str(df['time'])                                     
     df['month'] = df['date'].dt.month
     df['day'] = df['day'].map({'Monday': 1, 'Tuesday': 2, 'Wednesday': 3, 'Thursday': 4, 'Friday': 5, 'Saturday': 6, 'Sunday': 7})
-    # for i in range(len(df)):
-    #     df['traffic_time'][i] = int(str(df['traffic_time'][i])[:2])
-    # df['traffic_time'] = df['traffic_time'].str.split(':')[0]
-    # df['traffic_time'] = int(df['traffic_time'])
     df.drop('date', axis = 1, inplace=True)
     reorder = ['Junction','week','day','traffic_time','month']
     df = df[reorder]
